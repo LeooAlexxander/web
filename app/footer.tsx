7b45716ad3a08812fbd20e5d43f320eaa7ec3591
LeooAlexxander <leo.swatosch@icloud.com>
2025-05-06 16:08:47 +0200

    footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -71,8 +71,8 @@ export function Footer() {
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2025.</span>
-            <span>Leo Alexander.</span>
+            <span>© 2025</span>
+            <span>Leo Alexander</span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
