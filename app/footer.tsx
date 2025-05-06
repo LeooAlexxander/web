8137647c13fccc7fdec6b3c0fddea8d8dd24ba53
LeooAlexxander <leo.swatosch@icloud.com>
2025-05-06 19:01:20 +0200

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,8 @@
 'use client'
+
 import { AnimatedBackground } from '@/components/ui/animated-background'
 import { TextLoop } from '@/components/ui/text-loop'
-import { MonitorIcon, MoonIcon, SunIcon } from 'lucide-react'
+import { SunIcon, MoonIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
@@ -15,11 +16,6 @@ const THEMES_OPTIONS = [
     label: 'Dark',
     id: 'dark',
     icon: <MoonIcon className="h-4 w-4" />,
-  },
-  {
-    label: 'System',
-    id: 'system',
-    icon: <MonitorIcon className="h-4 w-4" />,
   },
 ]
 
@@ -39,29 +35,21 @@ function ThemeSwitch() {
     <AnimatedBackground
       className="pointer-events-none rounded-lg bg-zinc-100 dark:bg-zinc-800"
       defaultValue={theme}
-      transition={{
-        type: 'spring',
-        bounce: 0,
-        duration: 0.2,
-      }}
+      transition={{ type: 'spring', bounce: 0, duration: 0.2 }}
       enableHover={false}
-      onValueChange={(id) => {
-        setTheme(id as string)
-      }}
+      onValueChange={(id) => setTheme(id as string)}
     >
-      {THEMES_OPTIONS.map((theme) => {
-        return (
-          <button
-            key={theme.id}
-            className="inline-flex h-7 w-7 items-center justify-center text-zinc-500 transition-colors duration-100 focus-visible:outline-2 data-[checked=true]:text-zinc-950 dark:text-zinc-400 dark:data-[checked=true]:text-zinc-50"
-            type="button"
-            aria-label={`Switch to ${theme.label} theme`}
-            data-id={theme.id}
-          >
-            {theme.icon}
-          </button>
-        )
-      })}
+      {THEMES_OPTIONS.map((opt) => (
+        <button
+          key={opt.id}
+          className="inline-flex h-7 w-7 items-center justify-center text-zinc-500 transition-colors duration-100 focus-visible:outline-2 data-[checked=true]:text-zinc-950 dark:text-zinc-400 dark:data-[checked=true]:text-zinc-50"
+          type="button"
+          aria-label={`Switch to ${opt.label} theme`}
+          data-id={opt.id}
+        >
+          {opt.icon}
+        </button>
+      ))}
     </AnimatedBackground>
   )
 }
