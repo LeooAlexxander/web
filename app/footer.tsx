0aaa701d68465a0e60d5301f7619cfdef8a9335e
LeooAlexxander <leo.swatosch@icloud.com>
2025-05-06 16:33:13 +0200

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,11 +70,10 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-          <TextLoop className="text-xs text-zinc-500">
-            <span>© 2025</span>
-            <span>Leo Alexander</span>
-          </TextLoop>
-        </a>
+        <TextLoop className="text-xs text-zinc-500">
+          <span>© 2025</span>
+          <span>Leo Alexander</span>
+        </TextLoop>
         <div className="text-xs text-zinc-400">
           <ThemeSwitch />
         </div>
