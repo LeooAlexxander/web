94237e458a89bf2024f12135d28eda90c745e13e
LeooAlexxander <leo.swatosch@icloud.com>
2025-05-06 19:53:34 +0200

    video to image

--- app/data.ts
+++ app/data.ts
@@ -17,16 +17,14 @@ export const PROJECTS: Project[] = [
     description:
       'My YouTube channel is the place where I document my journey, share my learnings and make an impact on others doing that.',
     link: 'https://www.youtube.com/@LeoSquaretoes',
-    video:
-      'https://www.youtube.com/watch?v=QsMT07BXzlM&t=1s',
+    image: '/images/youtube.png',
     id: 'project1',
   },
   {
     name: 'Alchos AI',
     description: 'An AI app that helps you track your habits, achieve your goals, and do your affirmations.',
     link: 'https://x.com/LeooAlexxander',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    image: '/images/waitlist.png',
     id: 'project2',
   },
 ]
