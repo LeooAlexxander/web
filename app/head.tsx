65229ee6aa453bc129908417bb6fa35eea24f7aa
LeooAlexxander <leo.swatosch@icloud.com>
2025-05-08 16:52:48 +0200

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Leo Alexander</title>
+      <meta name="description" content="Portfolio, projects, and experiments by Leo Alexander." />
+      <link rel="icon" href="/Cognero.svg" type="image/svg+xml" />
+    </>
+  )
+}
