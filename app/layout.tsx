b9ddd7e6b28b7bda82aae12e8e4992e236525685
LeooAlexxander <leo.swatosch@icloud.com>
2025-05-08 16:47:15 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: 'Nim - Personal website template',
+  title: 'Leo Alexander',
   description:
-    'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+    'Leo Alexander is a pro.',
 }
 
 const geist = Geist({
